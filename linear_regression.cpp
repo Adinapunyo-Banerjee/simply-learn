a89ac9fa01fec26c5418b92beeb2e896371cb0e6
Adinapunyo Banerjee <adinapunyo@gmail.com>
2022-10-27 22:52:47 +0530

    Added cost function in linear_model

--- linear_regression.cpp
+++ linear_regression.cpp
@@ -11,16 +11,14 @@ class linear_model
     double w, b;
     int size = 0;
 
+    public:
     double cost(double w, double b)
     {
-        double error_squared = 0;
-        for(int i=0; i<size(); i++)
-        {
-            
-        }
+        double squared_error = 0;
+        for(int i=0; i<size; i++) squared_error += (w*feature[i]+b - label[i])*(w*feature[i]+b - label[i]);
+        return squared_error/(double)(2*size);
     }
 
-    public:
     linear_model(vector<double> feature, vector<double> label)
     {
         if(feature.size()==label.size())
@@ -29,6 +27,18 @@ class linear_model
             this->label = label;
             size = feature.size();
         }
+    }
+
+    bool initialise_data(vector<double> feature, vector<double> label)
+    {
+        if(feature.size()==label.size())
+        {
+            this->feature = feature;
+            this->label = label;
+            size = feature.size();
+            return true;
+        }
+        return false;
     }
 
     void show_data()
@@ -49,10 +59,10 @@ int main()
     vector<double> f = {1.0, 2.0, 3.0};
     vector<double> b = {1.0, 2.0, 3.0};
 
-    
-
     linear_model model(f, b);
     model.show_data();
+
+    cout<<model.cost(1, 0);
 
     return 0;
 }
