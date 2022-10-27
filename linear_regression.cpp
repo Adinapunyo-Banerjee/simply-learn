885d0bf92c0ab26798b732a6be9861763364c43e
Adinapunyo Banerjee <adinapunyo@gmail.com>
2022-10-28 00:05:47 +0530

    Completely working linear_model

--- linear_regression.cpp
+++ linear_regression.cpp
@@ -8,15 +8,50 @@ class linear_model
     private:
     vector<double> feature;
     vector<double> label;
+    bool trained = false;
     double w, b;
     int size = 0;
 
-    public:
     double cost(double w, double b)
     {
         double squared_error = 0;
         for(int i=0; i<size; i++) squared_error += (w*feature[i]+b - label[i])*(w*feature[i]+b - label[i]);
         return squared_error/(double)(2*size);
+    }
+
+    double derivative_sum_w(double w, double b)
+    {
+        double error = 0;
+        for(int i=0; i<size; i++) error += (w*feature[i]+b - label[i])*feature[i];
+        return error/(double)(size);
+    }
+
+    double derivative_sum_b(double w, double b)
+    {
+        double error = 0;
+        for(int i=0; i<size; i++) error += (w*feature[i]+b - label[i]);
+        return error/(double)(size);
+    }
+
+    public:
+    void gradient_descent(double learning_rate)
+    {
+        double w = 0, b = 0;
+        double change = 1;
+        double previous_cost = cost(w, b);
+        while(change>=0.00001)
+        {
+            double _w = w - learning_rate*derivative_sum_w(w, b);
+            double _b = b - learning_rate*derivative_sum_b(w, b);
+            change = previous_cost - cost(_w, _b);
+            previous_cost = cost(_w, _b);
+            w = _w;
+            b = _b;
+            cout<<w<<" "<<b<<" "<<cost(w, b)<<endl;
+        }
+        this->w = w;
+        this->b = b;
+        trained = true;
     }
 
     linear_model(vector<double> feature, vector<double> label)
@@ -27,6 +62,12 @@ class linear_model
             this->label = label;
             size = feature.size();
         }
+    }
+
+    double predict(double feature)
+    {
+        if(trained) return (this->w)*feature + (this->b);
+        return 0;
     }
 
     bool initialise_data(vector<double> feature, vector<double> label)
@@ -61,8 +102,9 @@ int main()
 
     linear_model model(f, b);
     model.show_data();
+    model.gradient_descent(0.0001);
 
-    cout<<model.cost(1, 0);
+    cout<<model.predict(1.5);
 
     return 0;
 }
